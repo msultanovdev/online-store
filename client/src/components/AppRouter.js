394f21f0d82d4b04a8c6a06d4682abac0042814b
msultanovdev <mukhammadamin.sultanov@gmail.com>
2022-06-11 17:28:30 +0500

    Bulding Router & UserStore

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Context } from '..';
+import Shop from '../pages/Shop';
+import { authRoutes, publicRoutes } from '../routes';
+
+const AppRouter = () => {
+    const {user} = useContext(Context);
+
+    return (
+        <Routes>
+            {user.isAuth && authRoutes.map(({path, Component}) => 
+                <Route key={path} path={path} element={<Component/>} exact/>
+            )}
+            {publicRoutes.map(({path, Component}) => 
+                <Route key={path} path={path} element={<Component/>} exact/>
+            )}
+            <Route path="*" element={<Shop/>} />
+        </Routes>
+    )
+}
+
+export default AppRouter;
