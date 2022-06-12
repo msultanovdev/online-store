902d194baa223dbbad38b73362ae3aa220759c52
msultanovdev <mukhammadamin.sultanov@gmail.com>
2022-06-12 22:03:37 +0500

    Building AUTH component

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -10,10 +10,10 @@ const AppRouter = () => {
     return (
         <Routes>
             {user.isAuth && authRoutes.map(({path, Component}) => 
-                <Route key={path} path={path} element={Component} exact/>
+                <Route key={path} path={path} element={<Component />} exact/>
             )}
             {publicRoutes.map(({path, Component}) => 
-                <Route key={path} path={path} element={Component} exact/>
+                <Route key={path} path={path} element={<Component />} exact/>
             )}
             <Route
                 path="*"
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,36 +1,36 @@
 import Admin from "./pages/Admin";
-import Auth from "./pages/Auth";
+import {ADMIN_ROUTE, BASKET_ROUTE, DEVICE_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE} from "./utils/consts";
 import Basket from "./pages/Basket";
-import DevicePage from "./pages/DevicePage";
 import Shop from "./pages/Shop";
-import { ADMIN_ROUTE, BASKET_ROUTE, DEVICE_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE } from "./utils/consts";
+import Auth from "./pages/Auth";
+import DevicePage from "./pages/DevicePage";
 
 export const authRoutes = [
     {
         path: ADMIN_ROUTE,
-        component: Admin,
+        Component: Admin
     },
     {
         path: BASKET_ROUTE,
-        component: Basket,
-    }
-];
+        Component: Basket
+    },
+]
 
 export const publicRoutes = [
     {
         path: SHOP_ROUTE,
-        component: Shop,
+        Component: Shop
     },
     {
         path: LOGIN_ROUTE,
-        component: Auth,
+        Component: Auth
     },
     {
         path: REGISTRATION_ROUTE,
-        component: Auth,
+        Component: Auth
     },
     {
         path: DEVICE_ROUTE + '/:id',
-        component: DevicePage,
-    }
-];
+        Component: DevicePage
+    },
+]
